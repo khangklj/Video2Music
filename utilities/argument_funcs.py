d7093a324860a7a7785f7819266e21120db7d97c
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2024-06-12 11:11:01 +0700

    Update argument_funcs.py

--- utilities/argument_funcs.py
+++ utilities/argument_funcs.py
@@ -108,7 +108,7 @@ def parse_eval_args():
     parser.add_argument("-input_dir_music", type=str, default="./dataset/vevo_chord/" + MUSIC_TYPE, help="Folder of video CNN feature files")
     parser.add_argument("-input_dir_video", type=str, default="./dataset/vevo_vis", help="Folder of video CNN feature files")
     
-    parser.add_argument("-model_weights", type=str, default= modelpath, help="Pickled model weights file saved with torch.save and model.state_dict()")
+    parser.add_argument("-model_weights", type=str, default= "./saved_models/AMT/epoch_0030.pickle", help="Pickled model weights file saved with torch.save and model.state_dict()")
     
     parser.add_argument("-n_workers", type=int, default=4, help="Number of threads for the dataloader")
     parser.add_argument("--force_cpu", type=bool, default=False, help="Forces model to run on a cpu even when gpu is available")

02e37d82e8c3b46bec8549be7506cfa457aa5758
Phan Trung Thuận <143245984+Phan-Trung-Thuan@users.noreply.github.com>
2024-06-15 11:32:26 +0700

    Update argument_funcs.py

--- utilities/argument_funcs.py
+++ utilities/argument_funcs.py
@@ -112,7 +112,7 @@ def parse_eval_args():
     
     parser.add_argument("-n_workers", type=int, default=4, help="Number of threads for the dataloader")
     parser.add_argument("--force_cpu", type=bool, default=False, help="Forces model to run on a cpu even when gpu is available")
-    parser.add_argument("-batch_size", type=int, default=1, help="Batch size to use")
+    parser.add_argument("-batch_size", type=int, default=32, help="Batch size to use")
     
     parser.add_argument("-max_sequence_midi", type=int, default=2048, help="Maximum midi sequence to consider")
     parser.add_argument("-max_sequence_video", type=int, default=300, help="Maximum video sequence to consider")
@@ -122,7 +122,7 @@ def parse_eval_args():
     parser.add_argument("-num_heads", type=int, default=8, help="Number of heads to use for multi-head attention")
     parser.add_argument("-d_model", type=int, default=512, help="Dimension of the model (output dim of embedding layers, etc.)")
     parser.add_argument("-dim_feedforward", type=int, default=1024, help="Dimension of the feedforward layer")
-    parser.add_argument('-music_gen_version', type=int, default=32, help="Version number. None is original musgic generation AMT model")
+    parser.add_argument('-music_gen_version', type=int, default=1, help="Version number. None is original musgic generation AMT model")
     parser.add_argument('-regression_version', type=int, default=None, help="Version number. None is original loudness and note density Regression model")
 
     parser.add_argument("-is_video", type=bool, default=IS_VIDEO, help="MusicTransformer or VideoMusicTransformer")

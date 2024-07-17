ff9fa898ddc8004126af41c88a811e80815c7591
khangklj <160006238+khangklj@users.noreply.github.com>
2024-07-17 08:37:40 +0700

    Update custom_lstm.py

--- model/custom_lstm.py
+++ model/custom_lstm.py
@@ -22,6 +22,7 @@ class LSTMCell(nn.Module):
 
     def forward(self, x, h, c):
         # x has shape [batch_size, seq_len, input_size]
+        # h and c each has shape [layer_num, batch_size, hidden_dim]
         output_hiddens, output_cells = [], []
         for i in range(x.shape[1]):
             forget_gate = F.sigmoid((x[:, i] @ self.forget_input) + (h @ self.forget_hidden) + self.forget_bias)
@@ -33,8 +34,6 @@ class LSTMCell(nn.Module):
             output_hiddens.append(h.unsqueeze(1))
             output_cells.append(c.unsqueeze(1))
         return torch.concat(output_hiddens, dim=1), torch.concat(output_cells, dim=1)
-
-
 
 class LSTM(nn.Module):
 
@@ -50,9 +49,14 @@ class LSTM(nn.Module):
         nn.init.xavier_uniform_(self.linear.weight.data)
         self.linear.bias.data.fill_(0.0)
 
-    def forward(self, x, h):
+    def forward(self, x, h=None):
         # x has shape [batch_size, seq_len, embed_dim]
         # h is a tuple containing h and c, each have shape [layer_num, batch_size, hidden_dim]
+        if h is None:
+            if torch.cuda.is_available():
+                h = (torch.zeros(self.num_layers, x.size(0), self.hidden_dim).cuda(), torch.zeros(self.num_layers, x.size(0), self.hidden_dim).cuda())
+            else
+                h = (torch.zeros(self.num_layers, x.size(0), self.hidden_dim), torch.zeros(self.num_layers, x.size(0), self.hidden_dim))
         hidden, cell = h
         output_hidden, output_cell = self.layers[0](x, hidden[0], cell[0])
         new_hidden, new_cell = [output_hidden[:, -1].unsqueeze(0)], [output_cell[:, -1].unsqueeze(0)]

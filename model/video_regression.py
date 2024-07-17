6a309cbfbc667081d1cc771c221e50930a85dac3
khangklj <160006238+khangklj@users.noreply.github.com>
2024-07-17 16:25:25 +0700

    Update video_regression.py

--- model/video_regression.py
+++ model/video_regression.py
@@ -1,174 +1,69 @@
 import torch
-import torch.nn as nn
-from torch.nn.modules.normalization import LayerNorm
-# from torchtune.modules import RMSNorm
-import random
-import numpy as np
-from utilities.constants import *
-from utilities.device import get_device
-from datetime import datetime
-from .moe import *
-from .custom_lstm import LSTM
-
+from torch import nn
 import torch.nn.functional as F
-    
-class advancedRNNBlock(nn.Module):
-    def __init__(self, rnn_type='gru', ff_type='mlp', d_model=256, d_hidden=1024, dropout=0.1, bidirectional=True):
-        super(advancedRNNBlock, self).__init__()
 
-        self.d_model = d_model
-        self.d_hidden = d_hidden
-        self.dropout = dropout
-        self.bidirectional = bidirectional
+class LSTMCell(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(LSTMCell, self).__init__()
+        self.input_dim, self.hidden_dim = input_dim, hidden_dim
+        self.forget_input, self.forget_hidden, self.forget_bias = self.create_gate_parameters()
+        self.input_input, self.input_hidden, self.input_bias = self.create_gate_parameters()
+        self.output_input, self.output_hidden, self.output_bias = self.create_gate_parameters()
+        self.cell_input, self.cell_hidden, self.cell_bias = self.create_gate_parameters()
 
-        if rnn_type == None:
-            self.rnn_layer = nn.RNN(self.d_model, self.d_model, num_layers=1, bidirectional=bidirectional, batch_first=True)
-        elif rnn_type == 'gru':
-            self.rnn_layer = nn.GRU(self.d_model, self.d_model, num_layers=1, bidirectional=bidirectional, batch_first=True)
-        elif rnn_type == 'lstm':
-            self.rnn_layer = nn.LSTM(self.d_model, self.d_model, num_layers=1, bidirectional=bidirectional, batch_first=True)
-    
-        if ff_type == 'mlp':
-            self.ff_layer = nn.Sequential(
-                nn.Linear(self.d_model * (2 if bidirectional else 1), self.d_hidden),
-                nn.SiLU(),
-                nn.Linear(self.d_hidden, self.d_model)
-            )
-        elif ff_type == 'moe':
-            expert = nn.Sequential(
-                GLUExpert(self.d_model * (2 if bidirectional else 1), self.d_hidden),
-                nn.Linear(self.d_model * (2 if bidirectional else 1), self.d_model)
-            )
-            
-            self.ff_layer = MoELayer(expert, self.d_model * (2 if bidirectional else 1), d_ff=self.d_hidden, n_experts=6, n_experts_per_token=1, dropout=dropout)
+    def create_gate_parameters(self):
+        input_weights = nn.Parameter(torch.zeros(self.input_dim, self.hidden_dim))
+        hidden_weights = nn.Parameter(torch.zeros(self.hidden_dim, self.hidden_dim))
+        nn.init.xavier_uniform_(input_weights)
+        nn.init.xavier_uniform_(hidden_weights)
+        bias = nn.Parameter(torch.zeros(self.hidden_dim))
+        return input_weights, hidden_weights, bias
 
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-        self.last_layer = nn.Linear(self.d_model * (2 if bidirectional else 1), self.d_model)
+    def forward(self, x, h, c):
+        # x has shape [batch_size, input_size]
+        # h and c each has shape [batch_size, hidden_dim]
+        forget_gate = F.sigmoid((x @ self.forget_input) + (h @ self.forget_hidden) + self.forget_bias)
+        input_gate = F.sigmoid((x @ self.input_input) + (h @ self.input_hidden) + self.input_bias)
+        output_gate = F.sigmoid((x @ self.output_input) + (h @ self.output_hidden) + self.output_bias)
+        input_activations = F.tanh((x @ self.cell_input) + (h @ self.cell_hidden) + self.cell_bias)
+        c = (forget_gate * c) + (input_gate * input_activations)
+        h = F.tanh(c) * output_gate
+        return h, c
 
-    def forward(self, x):
-        x_rnn, _ = self.rnn_layer(x)
-        x_double = torch.cat((x, x), dim=-1)
-        x = self.dropout1(x_rnn + x_double)
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, bidirectional=False, dropout=0.5):
+        super(LSTM, self).__init__()
+        self.input_dim, self.hidden_dim, self.num_layers, self.bidirectional = input_dim, hidden_dim, num_layers, bidirectional
+        self.layers = nn.ModuleList()
+        for _ in range(num_layers):
+            self.layers.append(LSTMCell(input_dim if len(self.layers) == 0 else hidden_dim * 2, hidden_dim))
+        self.dropout = nn.Dropout(dropout)
+        # self.linear = nn.Linear(hidden_dim * 2, input_dim)
+        nn.init.xavier_uniform_(self.linear.weight.data)
+        # self.linear.bias.data.fill_(0.0)
 
-        x_ff = self.ff_layer(x)
-        x_double = torch.cat((x, x), dim=-1)
-        print(x.shape, x_ff.shape, x_double.shape)
-        x = self.dropout2(x_ff + x_double)
-
-        x = self.last_layer(x)
-
-        return x
-
-class VideoRegression(nn.Module):
-    def __init__(self, n_layers=2, d_model=256, d_hidden=1024, dropout=0.1, max_sequence_video=300, total_vf_dim=0, regModel="bilstm"):
-        super(VideoRegression, self).__init__()
-        self.nlayers    = n_layers
-        self.d_model    = d_model
-        self.d_hidden = d_hidden
-        self.dropout    = dropout
-        self.max_seq_video    = max_sequence_video
-        self.total_vf_dim = total_vf_dim
-        self.regModel = regModel
-
-        if self.regModel == "bilstm":
-            self.bilstm = nn.LSTM(self.total_vf_dim, self.d_model, self.nlayers, bidirectional=True)
-        elif self.regModel == "bigru":
-            self.bigru = nn.GRU(self.total_vf_dim, self.d_model, self.nlayers, bidirectional=True)
-        elif self.regModel == "lstm":
-            self.lstm = nn.LSTM(self.total_vf_dim, self.d_model, self.nlayers)
-        elif self.regModel == "gru":
-            self.gru = nn.GRU(self.total_vf_dim, self.d_model, self.nlayers)
-        elif self.regModel == "version_1":
-            # First RNN layer (bidirectional)
-            self.bigru1 = nn.GRU(self.total_vf_dim, self.d_model, 1, bidirectional=True, batch_first=True)
-            
-            # First MLP layer
-            self.mlp1 = nn.Sequential(
-                nn.Linear(self.d_model * 2, self.d_hidden),
-                nn.SiLU(),
-                nn.Linear(self.d_hidden, self.total_vf_dim),
-                nn.Dropout(self.dropout)
-            )  
-            
-            # Second RNN layer (non-bidirectional)
-            self.bigru2 = nn.GRU(self.total_vf_dim, self.d_model, 1, bidirectional=True, batch_first=True)
-            
-            # Second MLP layer
-            self.mlp2 = nn.Sequential(
-                nn.Linear(self.d_model * 2, self.d_hidden),
-                nn.SiLU(),
-                nn.Linear(self.d_hidden, self.d_model * 2),
-                nn.Dropout(self.dropout)
-            )
-        elif self.regModel == "version_2":
-            self.model = nn.Sequential(
-                nn.Linear(self.total_vf_dim, self.d_model),
-                *[advancedRNNBlock('gru', 'mlp', d_model, d_hidden, dropout, bidirectional=True) for _ in range(n_layers)]
-            )
-        elif self.regModel == "version_3":
-            self.model = nn.Sequential(
-                nn.Linear(self.total_vf_dim, self.d_model),
-                *[advancedRNNBlock('gru', 'moe', d_model, d_hidden, dropout, bidirectional=True) for _ in range(n_layers)]
-            )
-        elif self.regModel == "custom_lstm":
-            self.model = LSTM(self.total_vf_dim, self.d_model, self.nlayers)
-        elif self.regModel == "custom_bilstm":
-            self.model = LSTM(self.total_vf_dim, self.d_model, self.nlayers, bidirectional=False)
-            self.bilstm = nn.LSTM(self.total_vf_dim, self.d_model, self.nlayers, bidirectional=True)
-            
-        self.bifc = nn.Linear(self.d_model * 2, 2)
-        self.fc = nn.Linear(self.d_model, 2)
-        
-    def forward(self, feature_semantic_list, feature_scene_offset, feature_motion, feature_emotion):
-        ### Video (SemanticList + SceneOffset + Motion + Emotion) (ENCODER) ###
-        vf_concat = feature_semantic_list[0].float()
-        for i in range(1, len(feature_semantic_list)):
-            vf_concat = torch.cat( (vf_concat, feature_semantic_list[i].float()), dim=2)            
-        
-        vf_concat = torch.cat([vf_concat, feature_scene_offset.unsqueeze(-1).float()], dim=-1) 
-        vf_concat = torch.cat([vf_concat, feature_motion.unsqueeze(-1).float()], dim=-1) 
-        vf_concat = torch.cat([vf_concat, feature_emotion.float()], dim=-1) 
-
-        vf_concat = vf_concat.permute(1,0,2)
-        vf_concat = F.dropout(vf_concat, p=self.dropout, training=self.training)
-        # print(vf_concat.shape)
-        
-        if self.regModel == "bilstm":
-            out, _ = self.bilstm(vf_concat)
-            out = out.permute(1,0,2)
-            out = self.bifc(out)
-        elif self.regModel == "bigru":
-            out, _ = self.bigru(vf_concat)
-            out = out.permute(1,0,2)
-            out = self.bifc(out)
-        elif self.regModel == "lstm":
-            out, _ = self.lstm(vf_concat)
-            out = out.permute(1,0,2)
-            out = self.fc(out)
-        elif self.regModel == "gru":
-            out, _ = self.gru(vf_concat)
-            out = out.permute(1,0,2)
-            out = self.fc(out)
-        elif self.regModel == "version_1":
-            vf_concat = vf_concat.permute(1,0,2)
-            gru1_out, _ = self.bigru1(vf_concat)
-            mlp1_out = self.mlp1(gru1_out)
-            gru2_out, _ = self.bigru2(mlp1_out)
-            out = self.mlp2(gru2_out)
-            out = self.bifc(out)
-        elif self.regModel == "version_2" or self.regModel == "version_3":
-            vf_concat = vf_concat.permute(1,0,2)
-            out = self.model(vf_concat)
-            # out = self.bifc(out)
-        elif self.regModel == "custom_lstm":
-            out, _ = self.model(vf_concat)
-            out = out.permute(1,0,2)
-            out = self.fc(out)
-        elif self.regModel == "custom_bilstm":
-            vf_concat = vf_concat.permute(1,0,2)
-            out, _ = self.model(vf_concat)
-            print(out.shape)
-            out = self.bifc(out)            
-        return out
-        
+    def forward(self, x, h=None):
+        # x has shape [batch_size, seq_len, input_size]
+        # h is a tuple containing h and c, each have shape [layer_num, batch_size, hidden_dim]
+        if h is None:
+            if torch.cuda.is_available():
+                h = (torch.zeros(self.num_layers, x.size(0), self.hidden_dim).cuda(),
+                     torch.zeros(self.num_layers, x.size(0), self.hidden_dim).cuda())
+            else:
+                h = (torch.zeros(self.num_layers, x.size(0), self.hidden_dim),
+                     torch.zeros(self.num_layers, x.size(0), self.hidden_dim))
+        hidden, cell = h
+        all_hidden, all_cell = [], []
+        for i in range(x.shape[1]):
+            output_hidden, output_cell = self.layers[0](x[:, i], hidden[0], cell[0])
+            all_hidden.append(output_hidden.unsqueeze(1))
+            all_cell.append(output_cell.unsqueeze(1))
+            new_hidden, new_cell = output_hidden.unsqueeze(0), output_cell.unsqueeze(0)
+            for j in range(1, self.num_layers):
+                output_hidden, output_cell = self.layers[j](self.dropout(output_hidden), hidden[j], cell[j])
+                new_hidden = torch.cat((new_hidden, output_hidden.unsqueeze(0)), dim=0)
+                new_cell = torch.cat((new_cell, output_cell.unsqueeze(0)), dim=0)
+            hidden, cell = new_hidden, new_cell
+        output_hidden = torch.cat(all_hidden, dim=1)
+        output_cell = torch.cat(all_cell, dim=1)
+        return self.dropout(output_hidden), (hidden, cell)

a7d3db8257e8ab8966b60a8f612d566429245695
khangklj <160006238+khangklj@users.noreply.github.com>
2024-07-17 08:51:20 +0700

    Update video_regression.py

--- model/video_regression.py
+++ model/video_regression.py
@@ -168,8 +168,7 @@ class VideoRegression(nn.Module):
         elif self.regModel == "custom_bilstm":
             vf_concat = vf_concat.permute(1,0,2)
             out, _ = self.model(vf_concat)
-            print(out.shape)
-            out = out.permute(1,0,2)
+            print(out.shape)            
             out = self.fc(out)
         return out
         

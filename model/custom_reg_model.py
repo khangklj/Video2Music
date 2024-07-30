2e708705ab6dc54e5df54f5e8bcd09b1a8d45437
khangklj <160006238+khangklj@users.noreply.github.com>
2024-07-30 18:18:16 +0700

    Update custom_reg_model.py

--- model/custom_reg_model.py
+++ model/custom_reg_model.py
@@ -57,7 +57,7 @@ class LSTMCell(nn.Module):
         self.h = None # Hidden state
         self.c = None # Cell state
         
-    def forward(self, x):
+    def forward(self, x, h, c):
         # x: (batch_size, input_dim) - a batch of tokens
         
         # h: (batch_size, hidden_dim) - a batch of hidden states
@@ -89,7 +89,7 @@ class LSTMCell(nn.Module):
         self.h = o * F.tanh(self.c)
         
         # return new hidden state
-        return self.h
+        return self.h, (self.h, self.c)
         
 class GRUCell(nn.Module):
     def __init__(self, input_dim, hidden_dim):
@@ -156,6 +156,7 @@ class myRNN(nn.Module):
         self.output_dim = output_dim
         self.num_layers = num_layers
         self.bidirectional = bidirectional
+        self.cell_name = cell_name
         
         # Forward layers
         self.forward_layers = nn.ModuleList()
@@ -183,13 +184,20 @@ class myRNN(nn.Module):
         for i in range(x.shape[1]):
             for j in range(self.num_layers):
                 if j == 0:
-                    h_forward = self.forward_layers[j](x[:, i, :])
+                    if self.cell_name == "lstm":
+                        output, (h_forward, c_forward) = self.forward[j](x[:, i, :], None, None)
+                    else:
+                        h_forward = self.forward_layers[j](x[:, i, :])
                 else:
-                    h_forward = self.forward_layers[j](h_forward)
-            print(h_forward.shape)
+                    if self.cell_name == "lstm":
+                        output, (h_forward, c_forward) = self.forward[j](x[:, i, :], h_forward, c_forward)
+                    else:
+                        h_forward = self.forward_layers[j](h_forward)
+                    
             out_forward.append(h_forward)
             
-        out = out_forward
+        # out = out_forward
+        out = output
                     
         # Backward
         if self.bidirectional == True:
@@ -206,7 +214,8 @@ class myRNN(nn.Module):
             
             out = [torch.cat((out_forward[i], out_backward[i]), dim=1) for i in range(len(x.shape[1]))]
         
-        out = [self.regressor(item) for item in out]
+        # out = [self.regressor(item) for item in out]
+        out = self.regressor(out)
         
         # Return a list of output for every tokens (many-to-many)
         return out

31ee9f4d31799f760d95edc55ad182c79d1097ff
khangklj <160006238+khangklj@users.noreply.github.com>
2024-07-30 20:45:42 +0700

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -35,7 +35,7 @@ logging.getLogger().addHandler(fh)
 
 # main
 def main( vm = "", isPrintArgs = True):
-    args = parse_eval_args()
+    args = parse_eval_args()[0]
 
     if isPrintArgs:
         print_eval_args(args)

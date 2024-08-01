fd0a8f75604f7bc5a0eea3f12d05bc0b4544b1b8
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-01 19:54:02 +0700

    Update video_regression.py

--- model/video_regression.py
+++ model/video_regression.py
@@ -191,7 +191,7 @@ class VideoRegression(nn.Module):
         elif self.regModel == "mamba":
             vf_concat = vf_concat.permute(1,0,2)
             vf_concat = self.fc2(vf_concat)
-            out = self.model(vf_concat)
+            # out = self.model(vf_concat)
             out, _ = self.model(vf_concat)
             out = self.fc(out)
             print(out.shape)

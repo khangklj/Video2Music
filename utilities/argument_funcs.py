208e9a0fa54da4a300f4de35231740dc74987928
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-03 21:52:52 +0700

    Update argument_funcs.py

--- utilities/argument_funcs.py
+++ utilities/argument_funcs.py
@@ -25,7 +25,7 @@ def parse_train_args():
     parser.add_argument("-lr", type=float, default=None, help="Constant learn rate. Leave as None for a custom scheduler.")
     parser.add_argument("-ce_smoothing", type=float, default=None, help="Smoothing parameter for smoothed cross entropy loss (defaults to no smoothing)")
     parser.add_argument("-batch_size", type=int, default=1, help="Batch size to use")
-    parser.add_argument("-epochs", type=int, default=20, help="Number of epochs to use")
+    parser.add_argument("-epochs", type=int, default=50, help="Number of epochs to use")
 
     parser.add_argument("-max_sequence_midi", type=int, default=2048, help="Maximum midi sequence to consider")
     parser.add_argument("-max_sequence_video", type=int, default=300, help="Maximum video sequence to consider")
@@ -39,7 +39,7 @@ def parse_train_args():
 
     parser.add_argument("-is_video", type=bool, default=IS_VIDEO, help="MusicTransformer or VideoMusicTransformer")
     parser.add_argument('-music_gen_version', type=int, default=None, help="Version number. None is original musgic generation AMT model")
-    parser.add_argument('-regModel', type=str, default='mamba', help="Version name. None is original loudness and note density Regression model")
+    parser.add_argument('-regModel', type=str, default='bigru', help="Version name. None is original loudness and note density Regression model")
 
     # regModel version name:
     # lstm
@@ -136,7 +136,7 @@ def parse_eval_args():
     parser.add_argument("-d_model", type=int, default=512, help="Dimension of the model (output dim of embedding layers, etc.)")
     parser.add_argument("-dim_feedforward", type=int, default=1024, help="Dimension of the feedforward layer")
     parser.add_argument('-music_gen_version', type=int, default=None, help="Version number. None is original musgic generation AMT model")
-    parser.add_argument('-regModel', type=str, default='mamba', help="Version number. None is original loudness and note density Regression model")
+    parser.add_argument('-regModel', type=str, default='bigru', help="Version number. None is original loudness and note density Regression model")
     parser.add_argument("-is_video", type=bool, default=IS_VIDEO, help="MusicTransformer or VideoMusicTransformer")
     
     # regModel version name:

7ba838d20190f6e28dc355459c40085e5aede741
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-04 19:19:56 +0700

    Update video_music_transformer.py

--- model/video_music_transformer.py
+++ model/video_music_transformer.py
@@ -82,13 +82,6 @@ class VideoMusicTransformer_V1(nn.Module):
         x_attr = self.embedding_attr(x_attr)
         x = x_root + x_attr
 
-        # tmp_list = list()
-        # for i in range(x.shape[0]):
-        #     tmp = torch.full((1, x.shape[1], 1), feature_key[i,0].item())
-        #     tmp_list.append(tmp)
-        #     print(tmp.shape)
-        # feature_key_padded = torch.cat(tmp_list, dim=0)
-
         feature_key_padded = torch.full((x.shape[0], x.shape[1], 1), feature_key.item())       
         feature_key_padded = feature_key_padded.to(get_device())
         x = torch.cat([x, feature_key_padded], dim=-1)
@@ -286,7 +279,15 @@ class VideoMusicTransformer(nn.Module):
         x_attr = self.embedding_attr(x_attr)
         x = x_root + x_attr
 
-        # feature_key_padded = feature_key.unsqueeze(1).repeat(1, x.shape[1], 1)
+        # print(feature_semantic_list.shape, feature_key.shape, feature_scene_offset.shape, feature_motion.shape, feature_emotion.shape)
+
+        # tmp_list = list()
+        # for i in range(x.shape[0]):
+        #     tmp = torch.full((1, x.shape[1], 1), feature_key[i,0].item())
+        #     tmp_list.append(tmp)
+        #     print(tmp.shape)
+        # feature_key_padded = torch.cat(tmp_list, dim=0)
+        
         feature_key_padded = torch.full((x.shape[0], x.shape[1], 1), feature_key.item())
         feature_key_padded = feature_key_padded.to(get_device())
         x = torch.cat([x, feature_key_padded], dim=-1)

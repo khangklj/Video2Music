9971b002b86f080fa068fd6fae3a6decd8f798d5
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-06 17:46:13 +0700

    Fix bug in video_music_transformer.py

--- model/video_music_transformer.py
+++ model/video_music_transformer.py
@@ -475,6 +475,7 @@ class VideoMusicTransformer(nn.Module):
         feature_key_padded = feature_key.repeat(x.shape[0], x.shape[1], 1)
         # feature_key_padded = torch.full((x.shape[0], x.shape[1], 1), feature_key.item())
         feature_key_padded = feature_key_padded.to(get_device())
+        print(x.shape, feature_key_padded.shape, feature_key.shape)
         x = torch.cat([x, feature_key_padded], dim=-1)
 
         xf = self.Linear_chord(x)

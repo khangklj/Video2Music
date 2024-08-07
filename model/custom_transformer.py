a8598ad964273cafd05b1246f9733a99c9ca51aa
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-07 14:00:54 +0700

    Fix bug RMSNorm

--- model/custom_transformer.py
+++ model/custom_transformer.py
@@ -6,10 +6,54 @@ from torch.nn.init import *
 
 from torch.nn.functional import linear, softmax, dropout
 
+# https://github.com/bzhangGo/rmsnorm/blob/master/rmsnorm_torch.py
+class RMSNorm(nn.Module):
+    def __init__(self, d, p=-1., eps=1e-8, bias=False):
+        """
+            Root Mean Square Layer Normalization
+        :param d: model size
+        :param p: partial RMSNorm, valid value [0, 1], default -1.0 (disabled)
+        :param eps:  epsilon value, default 1e-8
+        :param bias: whether use bias term for RMSNorm, disabled by
+            default because RMSNorm doesn't enforce re-centering invariance.
+        """
+        super(RMSNorm, self).__init__()
+
+        self.eps = eps
+        self.d = d
+        self.p = p
+        self.bias = bias
+
+        self.scale = nn.Parameter(torch.ones(d))
+        self.register_parameter("scale", self.scale)
+
+        if self.bias:
+            self.offset = nn.Parameter(torch.zeros(d))
+            self.register_parameter("offset", self.offset)
+
+    def forward(self, x):
+        if self.p < 0. or self.p > 1.:
+            norm_x = x.norm(2, dim=-1, keepdim=True)
+            d_x = self.d
+        else:
+            partial_size = int(self.d * self.p)
+            partial_x, _ = torch.split(x, [partial_size, self.d - partial_size], dim=-1)
+
+            norm_x = partial_x.norm(2, dim=-1, keepdim=True)
+            d_x = partial_size
+
+        rms_x = norm_x * d_x ** (-1. / 2)
+        x_normed = x / (rms_x + self.eps)
+
+        if self.bias:
+            return self.scale * x_normed + self.offset
+
+        return self.scale * x_normed
+
 class MyRMSNorm(Module):
     def __init__(self, dim, eps=1e-6, batch_first=False): # dim = (seq_len, d_model)
         super(MyRMSNorm, self).__init__()
-        self.norm = nn.RMSNorm(dim, eps)
+        self.norm = RMSNorm(dim, eps)
         self.batch_first = batch_first
 
     def forward(self, x):

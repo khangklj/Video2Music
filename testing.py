0156fb3de1969e8b4388942e2dbf1172492d177f
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-07 19:15:50 +0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -18,16 +18,17 @@ def main():
     input = torch.randn(1, 50, 128, requires_grad=True)
     target_ce = torch.empty(1, 50).random_(1)
     targe_bce = torch.empty(1, 50, 128).random_(1)
-       
-    output_ce = ce_loss(reshape_tensor(input), reshape_tensor(target_ce))
-    output_bce = bce_loss(reshape_tensor(input), reshape_tensor(targe_bce))
-    print("Original: ")
+
+    print("Original: ") 
+    output_ce = ce_loss(reshape_tensor(input), target_ce.flatten())
+    output_bce = bce_loss(reshape_tensor(input), reshape_tensor(targe_bce))    
     print("ce_loss = ", output_ce, "bce_loss = ", output_bce)
 
     print(SEPERATOR)
-    output_ce = ce_loss(reshape_tensor_2(input), reshape_tensor_2(target_ce))
-    output_bce = bce_loss(reshape_tensor_2(input), reshape_tensor(targe_bce))
+
     print("Modify: ")
+    output_ce = ce_loss(reshape_tensor_2(input), target_ce)
+    output_bce = bce_loss(reshape_tensor_2(input), reshape_tensor_2(targe_bce))    
     print("ce_loss = ", output_ce, "bce_loss = ", output_bce)
 
     print(SEPERATOR)
@@ -38,15 +39,16 @@ def main():
     target_ce = torch.empty(16, 50).random_(1)
     targe_bce = torch.empty(16, 50, 128).random_(1)
 
-    output_ce = ce_loss(reshape_tensor(input), reshape_tensor(target_ce))
-    output_bce = bce_loss(reshape_tensor(input), reshape_tensor(targe_bce))
-    print("Original: ")
+    print("Original: ") 
+    output_ce = ce_loss(reshape_tensor(input), target_ce.flatten())
+    output_bce = bce_loss(reshape_tensor(input), reshape_tensor(targe_bce))    
     print("ce_loss = ", output_ce, "bce_loss = ", output_bce)
 
     print(SEPERATOR)
-    output_ce = ce_loss(reshape_tensor_2(input), reshape_tensor_2(target_ce))
-    output_bce = bce_loss(reshape_tensor_2(input), reshape_tensor(targe_bce))
+
     print("Modify: ")
+    output_ce = ce_loss(reshape_tensor_2(input), target_ce)
+    output_bce = bce_loss(reshape_tensor_2(input), reshape_tensor_2(targe_bce))    
     print("ce_loss = ", output_ce, "bce_loss = ", output_bce)
 
 

8e1c34d70ca198940a68fce21d617f04403ae097
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-07 21:43:29 +0700

    Update train_regression.py

--- train_regression.py
+++ train_regression.py
@@ -124,27 +124,27 @@ def main( vm = "" , isPrintArgs = True ):
     train_loss_func = nn.MSELoss()
 
     ##### Lr Scheduler vs static lr #####
-    if(args.lr is None):
-        if(args.continue_epoch is None):
-            init_step = 0
-        else:
-            init_step = args.continue_epoch * len(train_loader)
-        lr = LR_DEFAULT_START
-        lr_stepper = LrStepTracker(args.d_model, SCHEDULER_WARMUP_STEPS, init_step)        
-    else:
-        lr = args.lr        
+    # if(args.lr is None):
+    #     if(args.continue_epoch is None):
+    #         init_step = 0
+    #     else:
+    #         init_step = args.continue_epoch * len(train_loader)
+    #     lr = LR_DEFAULT_START
+    #     lr_stepper = LrStepTracker(args.d_model, SCHEDULER_WARMUP_STEPS, init_step)        
+    # else:
+    #     lr = args.lr        
 
     ##### Optimizer #####
-    opt = Adam(model.parameters(), lr=lr, betas=(ADAM_BETA_1, ADAM_BETA_2), eps=ADAM_EPSILON)
+    # opt = Adam(model.parameters(), lr=lr, betas=(ADAM_BETA_1, ADAM_BETA_2), eps=ADAM_EPSILON)
     # opt = AdamW(model.parameters(), lr=lr, betas=(ADAM_BETA_1, ADAM_BETA_2), eps=ADAM_EPSILON)
     
-    if(args.lr is None):
-        lr_scheduler = LambdaLR(opt, lr_stepper.step)
-    else:
-        lr_scheduler = None        
+    # if(args.lr is None):
+    #     lr_scheduler = LambdaLR(opt, lr_stepper.step)
+    # else:
+    #     lr_scheduler = None        
 
     ##### Original code ####
-    # opt = Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)    
+    opt = Adam(model.parameters(), lr=5e-4, weight_decay=1e-5)    
     # lr_scheduler = None
 
     ##### Tracking best evaluation accuracy #####

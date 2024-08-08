5e25c60454790e976e93a5656a8b12f9cc21b361
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-08 22:54:43 +0700

    Nothing

--- model/moe.py
+++ model/moe.py
@@ -80,6 +80,7 @@ class SharedMoELayer(Module):
     def forward(self, x):
         gate_logits = self.gate(x)
         weights, selected_experts = torch.topk(gate_logits, self.n_experts_per_token)
+        print(selected_experts, gate_logits)
         weights = softmax(weights, dim=-1, dtype=torch.float).to(get_device())
         out = torch.zeros_like(x)
         for i, expert in enumerate(self.experts):

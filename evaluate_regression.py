cef6a557a6ff3a17ea4bd2af6865ed7a583aa3a2
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-10 13:29:01 +0700

    Fix bug

--- evaluate_regression.py
+++ evaluate_regression.py
@@ -83,7 +83,8 @@ def main( vm = "", isPrintArgs = True):
     max_sequence_video = args.max_sequence_video
     regModel = args.regModel
     model = VideoRegression(n_layers=n_layers, d_model=d_model, d_hidden=d_hidden, use_KAN=use_KAN, max_sequence_video=max_sequence_video, total_vf_dim=total_vf_dim, regModel=regModel).to(get_device())
-    
+    model.load_state_dict(args.model_weights)
+
     loss = nn.MSELoss()
 
     logging.info( f"VIS MODEL: {args.vis_models}" )

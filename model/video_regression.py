8d90e78e805c534119d978e8b63c0bfdc292a8b9
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-11 17:21:44 +0700

    Update video_regression.py

--- model/video_regression.py
+++ model/video_regression.py
@@ -130,11 +130,15 @@ class VideoRegression(nn.Module):
         
         self.fc2 = nn.Linear(self.total_vf_dim, 2)
         
-        if self.regModel in ('mamba', 'moemamba', 'mamba+', 'bimamba'):
+        if self.regModel in ('mamba', 'moemamba', 'mamba+'):
             # self.fc3 = KANLinear(self.total_vf_dim, self.d_model)
             # self.fc4 = KANLinear(self.d_model, 2)
             self.fc3 = nn.Linear(self.total_vf_dim, self.d_model)
             self.fc4 = nn.Linear(self.d_model, 2)
+
+        if self.regModel in ('bimamba'):
+            self.fc3 = nn.Linear(self.total_vf_dim, self.d_model)
+            self.fc4 = nn.Linear(self.d_model * 2, 2)
 
     def forward(self, feature_semantic_list, feature_scene_offset, feature_motion, feature_emotion):
         ### Video (SemanticList + SceneOffset + Motion + Emotion) (ENCODER) ###

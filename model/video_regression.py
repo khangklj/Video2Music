2921374eb6ef06c9a8e5b88ecfceba6553a325ed
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-12 19:16:47 +0700

    Update video_regression.py

--- model/video_regression.py
+++ model/video_regression.py
@@ -127,7 +127,7 @@ class VideoRegression(nn.Module):
         elif self.regModel == "bimamba+":
             config = MambaConfig(d_model=self.d_model, n_layers=1, use_KAN=use_KAN, bias=True, use_version=1)
             # current best: n_encoder_layer = 3
-            self.model = BiMambaEncoder(config, self.d_hidden, n_encoder_layers=4)
+            self.model = BiMambaEncoder(config, self.d_hidden, n_encoder_layers=6)
             
         self.bifc = nn.Linear(self.d_model * 2, 2)
         self.fc = nn.Linear(self.d_model, 2)

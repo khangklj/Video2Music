c00f0ef184ff2726c0a51bd3b4a13946f1713384
khangklj <160006238+khangklj@users.noreply.github.com>
2024-08-12 09:17:05 +0700

    Update bimamba.py

--- model/bimamba.py
+++ model/bimamba.py
@@ -10,7 +10,10 @@ class BiMambaEncoder(nn.Module):
 
         self.layers = nn.ModuleList()
         for i in range(n_encoder_layers):
-            self.layers.append(BiMambaEncoderLayer(config, dim_feedforward))
+            if config.use_version == 0:
+                self.layers.append(BiMambaEncoderLayer(config, dim_feedforward))
+            else if config.use_version == 1:
+                self.layers.append(BiMambaEncoderLayer_V1(config, dim_feedforward))
 
     def forward(self, x):
         for i in range(self.n_encoder_layers):
@@ -41,16 +44,44 @@ class BiMambaEncoderLayer(nn.Module):
 
         # Forward
         mamba_out_forward = self.mamba_forward(x)
-        mamba_out_forward = self.norm1(mamba_out_forward)
-        output_forward = self.feed_forward(mamba_out_forward) + mamba_out_forward
+        output_forward = self.feed_forward(self.norm1(mamba_out_forward)) + mamba_out_forward
 
         # Backward
         mamba_out_backward = self.mamba_backward(x_flip)
-        mamba_out_backward = self.norm2(mamba_out_backward)
-        output_backward = self.feed_forward(mamba_out_backward) + mamba_out_backward
+        output_backward = self.feed_forward(self.norm2(mamba_out_backward)) + mamba_out_backward
 
         # Combine output
         output = output_forward + output_backward
-        # output = torch.cat((output_forward, output_backward), dim=-1)
 
+        return output
+        
+class BiMambaEncoderLayer_V1(nn.Module):
+    def __init__(self, config: MambaConfig, dim_feedforward=1024):
+        super().__init__()
+        assert config.use_version == 1, "use_version should be 1 to use Mamba+"
+        self.config = config
+        self.mamba_forward = Mamba(config)
+        self.mamba_backward = Mamba(config)
+        self.d_ff = dim_feedforward
+        
+        self.norm = nn.LayerNorm(config.d_model)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(config.d_model, dim_feedforward),
+            nn.ReLU(),
+            nn.Linear(dim_feedforward, config.d_model)
+        )
+        
+    def forward(self, x):
+        x_flip = torch.flip(x, dims=[1])
+
+        # Forward
+        mamba_out_forward = self.mamba_forward(x)
+        
+        # Backward
+        mamba_out_backward = self.mamba_backward(x_flip)
+        
+        # Combine output
+        output = mamba_out_forward + mamba_out_backward
+        output = self.feed_forward(self.norm(output))) + output
+        
         return output

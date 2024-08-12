6210a00bcee69133402bf80cea2cfe945e858919
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-12 11:47:41 +0700

    Debug

--- utilities/run_model_vevo.py
+++ utilities/run_model_vevo.py
@@ -311,9 +311,9 @@ def eval_model(model, dataloader,
                     tgt_root = tgt_root.flatten()
                     tgt_attr = tgt_attr.flatten()
                     
-                    # tgt_emotion = tgt_emotion.squeeze()
-                    tgt_emotion = tgt_emotion.flatten().unsqueeze(1)
-
+                    tgt_emotion = tgt_emotion.squeeze()
+                    # tgt_emotion = tgt_emotion.flatten().unsqueeze(1)
+                    print(y.shape, tgt.shape, tgt_emotion.shape)
                     loss_chord = eval_loss_func.forward(y, tgt)
                     loss_emotion = eval_loss_emotion_func.forward(y, tgt_emotion)
                     total_loss = LOSS_LAMBDA * loss_chord + (1-LOSS_LAMBDA) * loss_emotion

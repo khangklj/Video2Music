ddb55c429c3f5f6e4fc78d857ecb74752773d239
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-12 18:08:01 +0700

    Undo eval_model in run_model_vevo

--- utilities/run_model_vevo.py
+++ utilities/run_model_vevo.py
@@ -283,18 +283,18 @@ def eval_model(model, dataloader,
                             feature_motion,
                             feature_emotion)
                     
-                    # sum_acc += float(compute_vevo_accuracy(y, tgt ))
-                    # cor = float(compute_vevo_correspondence(y, tgt, tgt_emotion, tgt_emotion_prob, EMOTION_THRESHOLD))
+                    sum_acc += float(compute_vevo_accuracy(y, tgt ))
+                    cor = float(compute_vevo_correspondence(y, tgt, tgt_emotion, tgt_emotion_prob, EMOTION_THRESHOLD))
                     
                     # ====
-                    sum_acc_tmp = 0.0
-                    for i in range(y.shape[0]):
-                        sum_acc_tmp += float(compute_vevo_accuracy(y[i], tgt[i] ))
-                        cor = float(compute_vevo_correspondence(y[i], tgt[i], tgt_emotion[i], tgt_emotion_prob[i], EMOTION_THRESHOLD))
-                    sum_acc_tmp /= y.shape[0]
-                    cor /= y.shape[0]
+                    # sum_acc_tmp = 0.0
+                    # for i in range(y.shape[0]):
+                    #     sum_acc_tmp += float(compute_vevo_accuracy(y[i], tgt[i] ))
+                    #     cor = float(compute_vevo_correspondence(y[i], tgt[i], tgt_emotion[i], tgt_emotion_prob[i], EMOTION_THRESHOLD))
+                    # sum_acc_tmp /= y.shape[0]
+                    # cor /= y.shape[0]
 
-                    sum_acc += sum_acc_tmp                    
+                    # sum_acc += sum_acc_tmp                    
                     # ====
 
                     if cor >= 0 :

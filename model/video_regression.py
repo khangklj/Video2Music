a6961a388cbe1677392e7df48724db16190e6001
Khang <khangklj@gmail.com>
2024-08-14 18:45:49 +0700

    modified:   model/video_regression.py

--- model/video_regression.py
+++ model/video_regression.py
@@ -103,10 +103,15 @@ class VideoRegression(nn.Module):
             config = MambaConfig(d_model=self.d_model, n_layers=1, dropout=dropout, use_KAN=use_KAN, bias=True, use_version=1)
             self.model = BiMambaEncoder(config, self.d_hidden, n_encoder_layers=self.n_layers)
             
-        self.bifc = nn.Linear(self.d_model * 2, 2)
-        self.fc = nn.Linear(self.d_model, 2)
-        
-        self.fc2 = nn.Linear(self.total_vf_dim, 2)
+        # IMPORTANT MODIFY 
+        # self.bifc = nn.Linear(self.d_model * 2, 2)
+        # self.fc = nn.Linear(self.d_model, 2)        
+        # self.fc2 = nn.Linear(self.total_vf_dim, 2)
+
+        if self.regModel in ('gru', 'lstm'):
+            self.fc = nn.Linear(self.d_model, 2)        
+        if self.regModel in ('bigru', 'bilstm'):
+            self.bifc = nn.Linear(self.d_model * 2, 2)
         
         projection = nn.Linear
         # projection = KANLinear

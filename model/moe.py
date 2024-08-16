24ae6431c078e689a22c85f4d1b90014b50c0fe2
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-16 16:44:41 +0700

    Fix bug

--- model/moe.py
+++ model/moe.py
@@ -100,7 +100,7 @@ class SBRN(Module):
 
     def _routing(self, x, k=2):
         gate_logits = self.router(x)
-        weights, selected_experts = torch.topk(gate_logits, k).to(get_device())
+        weights, selected_experts = torch.topk(gate_logits, k)
         return weights, selected_experts
 
     def forward(self, x, k=2, t=1.0):

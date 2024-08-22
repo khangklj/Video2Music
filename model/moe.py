981887ea4e702bb28ceff283484703ffaf76b78b
Phan Trung Thuận <thuantk000@gmail.com>
2024-08-22 18:45:33 +0700

    Fix bug

--- model/moe.py
+++ model/moe.py
@@ -85,7 +85,7 @@ class ShannonEntropy(Module):
     def forward(self, x: Tensor): # x.shape = (batch_size, n)
         x /= x.sum(dim=1) # get probability
         x += self.eps # prevent log(0)
-        entropy = -torch.sum(x * (torch.log(x) / torch.log(torch.Tensor(6))), dim=1)
+        entropy = -torch.sum(x * (torch.log(x) / 1.79175946923), dim=1) # 1.79175946923 = ln(6)
         return entropy.mean()
 
 # Self-Balance Routing Network

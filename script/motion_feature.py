fee683e8f8ce90c022f18e5694516075091a689d
Phan Trung Thuận <thuantk000@gmail.com>
2024-09-21 12:06:24 +0700

    Update motion_feature.py

--- script/motion_feature.py
+++ script/motion_feature.py
@@ -6,6 +6,7 @@ import torchvision.models as models
 from tqdm import tqdm
 import torch
 from PIL import Image
+import clip
 
 TORCH_CPU_DEVICE = torch.device("cpu")
 
@@ -38,6 +39,7 @@ def main():
     directory_vevo = "../dataset/vevo/"
     datadict = {}
 
+    # === Our option 1 === #
     model = models.maxvit_t(weights=models.MaxVit_T_Weights.DEFAULT)   
     model.classifier = torch.nn.Sequential(
         torch.nn.AdaptiveAvgPool2d(1),
@@ -46,6 +48,9 @@ def main():
     model = model.to(get_device())
     model.eval()
     transform = models.MaxVit_T_Weights.IMAGENET1K_V1.transforms()
+
+    # === Our option 2 === #
+    # model, preprocess = clip.load("ViT-L/14@336px", device=get_device())
 
     for filename in tqdm(sorted(os.listdir(directory_vevo))):
         print(filename)
@@ -92,7 +97,7 @@ def main():
         #         f.write(str(i) + " "+motiondict[i]+"\n")
 
         # === Our option 1 === #
-        features = []
+        features = [np.zeros(512)]
         while cap.isOpened():
             # Read the frame and get its time stamp
             ret, frame = cap.read()
@@ -124,6 +129,40 @@ def main():
         features = np.stack(features, axis=0)
         print(features.shape)
         np.save("../dataset/vevo_motion/option1/" + fname + ".npy", features)
+
+        # === Our option 2 === #
+        # features = []
+        # while cap.isOpened():
+        #     # Read the frame and get its time stamp
+        #     ret, frame = cap.read()
+        #     if not ret:
+        #         break
+        #     curr_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+            
+        #     # Calculate the RGB difference between consecutive frames per second
+        #     if prev_frame is not None and curr_time - prev_time >= 1:
+        #         diff = cv2.absdiff(frame, prev_frame)
+        #         diff_rgb = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
+
+        #         diff_image = transform(Image.fromarray(diff_rgb)).unsqueeze(0).to(get_device())
+        #         with torch.no_grad():
+        #             motion_features = model(diff_image).squeeze()
+
+        #         # print("Motion value for second {}: {}".format(int(curr_time), motion_features.shape))
+        #         motion_features = motion_features.cpu().numpy()
+        #         features.append(motion_features)
+
+        #         prev_time = int(curr_time)
+
+        #     # Update the variables
+        #     prev_frame = frame.copy()
+        # # Release the video file and close all windows
+        # cap.release()
+        # cv2.destroyAllWindows()
+
+        # features = np.stack(features, axis=0)
+        # print(features.shape)
+        # np.save("../dataset/vevo_motion/option2/" + fname + ".npy", features)
 
 if __name__ == "__main__":
     main()

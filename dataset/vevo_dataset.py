596159b8be01023d3dc54d0eae6b698cbc811988
Phan Trung Thuận <thuantk000@gmail.com>
2024-10-13 21:04:39 +0700

    debug

--- dataset/vevo_dataset.py
+++ dataset/vevo_dataset.py
@@ -557,7 +557,8 @@ class VevoDataset(Dataset):
                 sample1[key] = self.paddingOrCutting(sample1[key], padding_dim=padding_dim, target_size=self.max_seq_video)
                 sample2[key] = self.paddingOrCutting(sample2[key], padding_dim=padding_dim, target_size=self.max_seq_video)
 
-            print(sample1[key].shape, sample2[key].shape)
+            if key == 'emotion':
+                print(sample1[key].shape, sample2[key].shape)
         return sample1, sample2
 
     def __getitem__(self, idx):

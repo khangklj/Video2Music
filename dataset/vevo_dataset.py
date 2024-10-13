709541232e680797775a7a2fc1a200c00662c01f
Phan Trung Thuận <thuantk000@gmail.com>
2024-10-13 21:45:38 +0700

    fix bug

--- dataset/vevo_dataset.py
+++ dataset/vevo_dataset.py
@@ -483,9 +483,9 @@ class VevoDataset(Dataset):
                 "motion": feature_motion,
                 "emotion": feature_emotion,
                 "tgt_emotion" : tgt_emotion,
-                "tgt_emotion_prob" : tgt_emotion_prob,
-                # "note_density" : feature_note_density,
-                # "loudness" : feature_loudness
+                # "tgt_emotion_prob" : tgt_emotion_prob,
+                "note_density" : feature_note_density,
+                "loudness" : feature_loudness
                 }
 
     def find_most_centered(self, nums, center_num=1.0):

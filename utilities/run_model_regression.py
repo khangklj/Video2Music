08150ee5ffcced2c63391d6cecac74a12deaedbc
Phan Trung Thuận <thuantk000@gmail.com>
2024-10-14 10:43:10 +0700

    fix bug

--- utilities/run_model_regression.py
+++ utilities/run_model_regression.py
@@ -71,9 +71,7 @@ def eval_model(model, dataloader, loss):
         sum_rmse_loudness = 0.0
 
         for batch in dataloader:
-            feature_semantic_list = [] 
-            for feature_semantic in batch["semanticList"]:
-                feature_semantic_list.append( feature_semantic.to(get_device()) )
+            feature_semantic_list = batch["semanticList"].to(get_device())
 
             feature_scene_offset = batch["scene_offset"].to(get_device())
             feature_motion = batch["motion"].to(get_device())

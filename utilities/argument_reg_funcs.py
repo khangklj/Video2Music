16279d11f9d0d26f089e3fc5eb5f466dfb926e58
Khang <khangklj@gmail.com>
2024-10-15 12:51:35 +0700

    modified:   utilities/argument_reg_funcs.py

--- utilities/argument_reg_funcs.py
+++ utilities/argument_reg_funcs.py
@@ -4,12 +4,12 @@ from .constants import *
 version = VERSION
 split_ver = SPLIT_VER
 split_path = "split_" + split_ver
-regModel = 'moe_bimamba+'
+regModel = 'bigru'
 augmentation = False
 chord_embedding = False
 batch_size = 32
 epochs = 50
-motion_type = 2
+motion_type = 1
 scene_embed = False
 
 def parse_train_args():

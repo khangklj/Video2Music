86abc1d8ec6f62eb9a38f2c19b04bc73d6180816
Khang <khangklj@gmail.com>
2024-10-20 14:30:48 +0700

    modified:   script/download_video.py

--- script/download_video.py
+++ script/download_video.py
@@ -2,15 +2,22 @@ from moviepy.editor import VideoFileClip
 import os
 from function import *
 from tqdm import tqdm
+import argparse
 
-def main():
-    video_dir_path = './dataset/video'
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Download YouTube videos.')
+    parser.add_argument('--format', type=str, default=1, help='Format of the idList.txt')
+    parser.add_argument('--video_dir_path', type=str, default='./dataset/video', help='Path to save downloaded videos')
+    return parser.parse_args()
+
+def main(video_dir_path='./dataset/video', format=1):
+    video_dir_path = video_dir_path
             
     # If the directory is not exist then create it
     if not os.path.exists(video_dir_path):
         os.makedirs(video_dir_path)
 
-    idList = get_id_list(idlist_path='./dataset/vevo_meta/idlist.txt')
+    idList = get_id_list(idlist_path='./dataset/vevo_meta/idlist.txt', format=format)
 
     # Download videos
     for index, id in tqdm(idList):
@@ -21,4 +28,5 @@ def main():
             download_youtube_video(id, video_dir_path, f'{index}.mp4')
 
 if __name__ == '__main__':
-    main()
+    args = parse_arguments()
+    main(args.video_dir_path, args.format)

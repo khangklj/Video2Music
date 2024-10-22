1c720fe239a940e0377e9861e407482837c1f695
Khang <khangklj@gmail.com>
2024-10-22 15:45:38 +0700

    Change to original

--- script/motion_feature.py
+++ script/motion_feature.py
@@ -50,7 +50,7 @@ def main():
     # transform = models.MaxVit_T_Weights.IMAGENET1K_V1.transforms()
 
     # === Our option 2 === #
-    model, preprocess = clip.load("ViT-L/14@336px", device=get_device())
+    # model, preprocess = clip.load("ViT-L/14@336px", device=get_device())
 
     for filename in tqdm(sorted(os.listdir(directory_vevo))):
         print(filename)
@@ -69,36 +69,36 @@ def main():
         sec = 0
         motiondict = {}
 
-        # while cap.isOpened():
-        #     # Read the frame and get its time stamp
-        #     ret, frame = cap.read()
-        #     if not ret:
-        #         break
-        #     curr_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+        while cap.isOpened():
+            # Read the frame and get its time stamp
+            ret, frame = cap.read()
+            if not ret:
+                break
+            curr_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
             
-        #     # Calculate the RGB difference between consecutive frames per second
-        #     motiondict[0] = "0.0000"
+            # Calculate the RGB difference between consecutive frames per second
+            motiondict[0] = "0.0000"
 
-        #     if prev_frame is not None and curr_time - prev_time >= 1:
-        #         diff = cv2.absdiff(frame, prev_frame)
-        #         diff_rgb = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
+            if prev_frame is not None and curr_time - prev_time >= 1:
+                diff = cv2.absdiff(frame, prev_frame)
+                diff_rgb = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
 
-        #         motion_value = diff_rgb.mean()
-        #         #print("Motion value for second {}: {}".format(int(curr_time), motion_value))
-        #         motion_value = format(motion_value, ".4f")
-        #         motiondict[int(curr_time)] = str(motion_value)
-        #         prev_time = int(curr_time)
+                motion_value = diff_rgb.mean()
+                #print("Motion value for second {}: {}".format(int(curr_time), motion_value))
+                motion_value = format(motion_value, ".4f")
+                motiondict[int(curr_time)] = str(motion_value)
+                prev_time = int(curr_time)
 
-        #     # Update the variables
-        #     prev_frame = frame.copy()
-        # # Release the video file and close all windows
-        # cap.release()
-        # cv2.destroyAllWindows()
+            # Update the variables
+            prev_frame = frame.copy()
+        # Release the video file and close all windows
+        cap.release()
+        cv2.destroyAllWindows()
 
-        # fpathname = "../dataset/vevo_motion/all/" + fname + ".lab"
-        # with open(fpathname,'w',encoding = 'utf-8') as f:
-        #     for i in range(0, len(motiondict)):
-        #         f.write(str(i) + " "+motiondict[i]+"\n")
+        fpathname = "../dataset/vevo_motion/all/" + fname + ".lab"
+        with open(fpathname,'w',encoding = 'utf-8') as f:
+            for i in range(0, len(motiondict)):
+                f.write(str(i) + " "+motiondict[i]+"\n")
 
         # === Our option 1 === #
         # features = [np.zeros(512)]
@@ -135,49 +135,49 @@ def main():
         # np.save("../dataset/vevo_motion/option1/" + fname + ".npy", features)
 
         # === Our option 2 === #
-        features = [np.zeros(768)]
-        residual_frames = []
-        while cap.isOpened():
-            # Read the frame and get its time stamp
-            ret, frame = cap.read()
-            if not ret:
-                break
-            curr_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+        # features = [np.zeros(768)]
+        # residual_frames = []
+        # while cap.isOpened():
+        #     # Read the frame and get its time stamp
+        #     ret, frame = cap.read()
+        #     if not ret:
+        #         break
+        #     curr_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
             
-            # Calculate the RGB difference between consecutive frames per second
-            if prev_frame is not None and curr_time - prev_time >= 1:
-                diff = cv2.absdiff(frame, prev_frame)
-                diff_rgb = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
+        #     # Calculate the RGB difference between consecutive frames per second
+        #     if prev_frame is not None and curr_time - prev_time >= 1:
+        #         diff = cv2.absdiff(frame, prev_frame)
+        #         diff_rgb = cv2.cvtColor(diff, cv2.COLOR_BGR2RGB)
 
-                while len(residual_frames) < 5:
-                    residual_frames.insert(0, np.zeros(diff_rgb.shape))
+        #         while len(residual_frames) < 5:
+        #             residual_frames.insert(0, np.zeros(diff_rgb.shape))
 
-                residual_frames.append(diff_rgb)
-                residual_frames.pop(0)
+        #         residual_frames.append(diff_rgb)
+        #         residual_frames.pop(0)
 
-                motion_image = np.zeros(diff_rgb.shape)
-                for i in range(5):
-                    motion_image += residual_frames[i] * 1/2**(4-i)
+        #         motion_image = np.zeros(diff_rgb.shape)
+        #         for i in range(5):
+        #             motion_image += residual_frames[i] * 1/2**(4-i)
 
-                motion_image = preprocess(Image.fromarray(motion_image.astype(np.uint8))).unsqueeze(0).to(get_device())
-                with torch.no_grad():
-                    motion_features = model.encode_image(motion_image).squeeze()
+        #         motion_image = preprocess(Image.fromarray(motion_image.astype(np.uint8))).unsqueeze(0).to(get_device())
+        #         with torch.no_grad():
+        #             motion_features = model.encode_image(motion_image).squeeze()
 
-                # print("Motion value for second {}: {}".format(int(curr_time), motion_features.shape))
-                motion_features = motion_features.cpu().numpy()
-                features.append(motion_features)
+        #         # print("Motion value for second {}: {}".format(int(curr_time), motion_features.shape))
+        #         motion_features = motion_features.cpu().numpy()
+        #         features.append(motion_features)
 
-                prev_time = int(curr_time)
+        #         prev_time = int(curr_time)
 
-            # Update the variables
-            prev_frame = frame.copy()
-        # Release the video file and close all windows
-        cap.release()
-        cv2.destroyAllWindows()
+        #     # Update the variables
+        #     prev_frame = frame.copy()
+        # # Release the video file and close all windows
+        # cap.release()
+        # cv2.destroyAllWindows()
 
-        features = np.stack(features, axis=0)
-        print(features.shape)
-        np.save("../dataset/vevo_motion/option2/" + fname + ".npy", features)
+        # features = np.stack(features, axis=0)
+        # print(features.shape)
+        # np.save("../dataset/vevo_motion/option2/" + fname + ".npy", features)
 
 if __name__ == "__main__":
     main()

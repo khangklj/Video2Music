4a5af742959890ddd6fccdf0288f8cfdfdbecbb1
Phan Trung Thuận <thuantk000@gmail.com>
2024-11-09 15:22:18 +0700

    debug

--- model/moe.py
+++ model/moe.py
@@ -240,13 +240,14 @@ class SharedMoELayer(Module):
         else:
             b = self.bias.T
             weights, selected_experts = torch.topk(gate_logits + b, k)
-            print(x.shape, gate_logits.shape, b.shape, (gate_logits + b).shape)
+            # print(x.shape, gate_logits.shape, b.shape, (gate_logits + b).shape)
 
             # Only get gate_logits
+            print(weights.shape, selected_experts.shape, b[selected_experts].shape)
             weights -= b[selected_experts].squeeze()
 
-            print(selected_experts)
-            print(selected_experts.shape)
+            # print(selected_experts)
+            # print(selected_experts.shape)
             c = torch.bincount(selected_experts.flatten()).to(self.bias.dtype)
             c_mean = torch.mean(c)
 

b2fd358eadec8ea097a7075d703b3c9646badd41
Phan Trung Thuận <thuantk000@gmail.com>
2024-11-11 15:15:04 +0700

    fix bug

--- model/rotate_operation.py
+++ model/rotate_operation.py
@@ -139,17 +139,13 @@ class RotaryPositionalEmbeddings(nn.Module):
         # reshape input; the last dimension is used for computing the output.
         # Cast to float to match the reference implementation
         # tensor has shape [b, s, n_h, h_d // 2, 2]
-        print(x.shape)
         xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
-        print(xshaped.shape)
 
         # reshape the cache for broadcasting
         # tensor has shape [b, s, 1, h_d // 2, 2] if packed samples,
         # otherwise has shape [1, s, 1, h_d // 2, 2]
-        print(rope_cache.shape)
         rope_cache = rope_cache.view(-1, xshaped.size(1), 1, xshaped.size(3), 2)
-        print(rope_cache.shape)
-        # rope_cache = rope_cache[:xshaped.size(2), ...]
+        rope_cache = rope_cache[:xshaped.size(0), ...]
 
         # tensor has shape [b, s, n_h, h_d // 2, 2]
         x_out = torch.stack(

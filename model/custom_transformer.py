814cafb6e7f920395eeace545b54ccee20f5e82c
Phan Trung Thuận <thuantk000@gmail.com>
2024-11-11 15:36:12 +0700

    debug

--- model/custom_transformer.py
+++ model/custom_transformer.py
@@ -1250,7 +1250,7 @@ class MultiheadGQA(Module):
         k = k.view(bsz, src_len, self.kv_heads*head_dim)
         v = v.view(bsz, src_len, self.kv_heads*head_dim)
 
-        print(q.shape, k.shape, v.shape)
+        # print(q.shape, k.shape, v.shape)
 
         # Unfold 'd' dimension into 'h' separate attention heads.
         q = rearrange(q, "b n (h d) -> b n h d", h=self.query_heads)

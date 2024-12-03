0ae420b6b674f201401b2637300e6a1c9074d818
Khang <khangklj@gmail.com>
2024-12-03 08:57:59 +0700

    Debug

--- model/moe.py
+++ model/moe.py
@@ -262,11 +262,15 @@ class SharedMoELayer(Module):
                 # print(f"x shape: {x.shape}")
                 # print(f"gate_logits shape: {gate_logits.shape}")
                 # print(f"selected_experts shape: {selected_experts.shape}")                
-                _, a = torch.topk(gate_logits, k)
-                _, b = torch.topk(gate_logits + b, k)
-                c_a = torch.bincount(a.flatten(), minlength=6).to(self.bias.dtype)
-                c_b = torch.bincount(b.flatten(), minlength=6).to(self.bias.dtype)
-                print(f"c_a: {c_a}; c_b: {c_b}")
+                                
+                print(f"gate_logits: {gate_logits[0][:10]}")
+
+                # _, a = torch.topk(gate_logits, k)
+                # _, b = torch.topk(gate_logits + b, k)
+                # c_a = torch.bincount(a.flatten(), minlength=6).to(self.bias.dtype)
+                # c_b = torch.bincount(b.flatten(), minlength=6).to(self.bias.dtype)
+                # print(f"c_a: {c_a}; c_b: {c_b}")
+
                 # print(f"c: {c}; c_mean: {c_mean}")                                
                 print(f"bias: {self.bias}")                
                 print("------END-------")

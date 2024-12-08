2d8db8901b4b7aab988da81b002ee0bf036f6aa5
Phan Trung Thuận <thuantk000@gmail.com>
2024-12-08 18:31:29 +0700

    update

--- utilities/argument_funcs.py
+++ utilities/argument_funcs.py
@@ -15,7 +15,7 @@ dropout = 0.2
 droptoken = 0.0
 lr = None
 optimizer = 'AdamW' # Adam / AdamW / RAdamW / Lion
-auxiliary_loss = True # False / True
+auxiliary_loss = False # False / True
 
 def parse_train_args():
     parser = argparse.ArgumentParser()

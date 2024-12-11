aa8cab68f230bbe5fba5e2bc205e0cc209038eaa
Phan Trung Thuận <thuantk000@gmail.com>
2024-12-11 22:04:41 +0700

    fix bug

--- model/rotate_operation.py
+++ model/rotate_operation.py
@@ -145,9 +145,10 @@ class RotaryPositionalEmbeddings(nn.Module):
         # reshape the cache for broadcasting
         # tensor has shape [b, s, 1, h_d // 2, 2] if packed samples,
         # otherwise has shape [1, s, 1, h_d // 2, 2]
+        print(xshaped.shape, rope_cache.shape)
         rope_cache = rope_cache.view(xshaped.size(0), xshaped.size(1), 1, xshaped.size(3), 2)
         # rope_cache = rope_cache[:xshaped.size(0), ...]
-        print(xshaped.shape, rope_cache.shape)
+        # print(xshaped.shape, rope_cache.shape)
 
         # tensor has shape [b, s, n_h, h_d // 2, 2]
         x_out = torch.stack(

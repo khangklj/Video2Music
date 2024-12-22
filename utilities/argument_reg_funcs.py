3404956e81e527ec955bc3613e003aea73a94ba5
khangklj <160006238+khangklj@users.noreply.github.com>
2024-12-22 16:50:58 +0700

    Update argument_reg_funcs.py

--- utilities/argument_reg_funcs.py
+++ utilities/argument_reg_funcs.py
@@ -4,12 +4,12 @@ from .constants import *
 version = VERSION
 split_ver = SPLIT_VER
 split_path = "split_" + split_ver
-regModel = 'bimamba+'
+regModel = 'lstm'
 augmentation = False
 chord_embedding = False
 batch_size = 64
 epochs = 100
-n_layers = 7
+n_layers = 6
 motion_type = 1
 lr = None
 scene_embed = False

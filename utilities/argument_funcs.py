489921b1e25e688d3dd236f77ef78c890c6f2d4f
Phan Trung Thuận <thuantk000@gmail.com>
2024-12-24 17:25:59 +0700

    update

--- utilities/argument_funcs.py
+++ utilities/argument_funcs.py
@@ -5,19 +5,19 @@ version = VERSION
 rpr = True
 augmentation = False
 chord_embed = True
-music_gen_version = '2.1'
+music_gen_version = '2.2'
 batch_size = 24
-epochs = 65
+epochs = 50
 motion_type = 1
 split_ver = SPLIT_VER
 split_path = "split_" + split_ver
 dropout = 0.2
 droptoken = 0.0
 lr = None
-optimizer = 'RAdamW' # Adam / AdamW / RAdamW / Lion
+optimizer = 'AdamW' # Adam / AdamW / RAdamW / Lion
 auxiliary_loss = True # False / True
 drop_loss = True # False / True
-balancing = True # True / False
+balancing = False # True / False
 
 def parse_train_args():
     parser = argparse.ArgumentParser()

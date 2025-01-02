2c7a1dc9ef09fed691287ba59ac16d0ab5e41f4d
Phan Trung Thuận <thuantk000@gmail.com>
2025-01-02 16:27:15 +0700

    update key detection

--- train_eval_key_detection.py
+++ train_eval_key_detection.py
@@ -36,7 +36,7 @@ split_ver = SPLIT_VER
 def create_sample(sample, model, X, y):
     semantic = sample['semanticList'].unsqueeze(0)
     emotion = sample['emotion'].unsqueeze(0)
-    feature = torch.hstack((semantic, emotion)).squeeze()
+    feature = torch.cat((semantic, emotion), dim=-1).squeeze()
     # feature = model.get_feature(semantic, None, None, emotion).squeeze().mean(dim=0)
     # feature = model.get_feature(semantic, None, None, emotion).squeeze()[0, :]
     # feature = model.get_feature(semantic, None, None, emotion).squeeze()[:5, :].flatten()

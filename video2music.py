5fd331345689f1c1eae390babf53752514c17e64
Phan Trung Thuận <thuantk000@gmail.com>
2025-01-06 22:38:23 +0700

    fix bug

--- video2music.py
+++ video2music.py
@@ -783,10 +783,10 @@ class Video2music:
 
             feature_emotion = feature_emotion.permute(1, 0, 2)
             window_size = 5
-            avg_kernel = torch.ones(1, 1, window_size) / window_size
+            avg_kernel = torch.ones(1, 1, window_size).to(get_device()) / window_size
             feature_emotion = torch.nn.functional.conv1d(feature_emotion, avg_kernel, padding=window_size//2)
             feature_emotion = feature_emotion.permute(1, 0, 2).squeeze()
-            emotion_indice = torch.argmax(feature_emotion, dim=1)
+            emotion_indice = torch.argmax(feature_emotion, dim=1).cpu()
 
             velolistExp = []
             exponent = 0.3

d79cace9f1aac023e8108520a7a97fd032e831b0
Khang <khangklj@gmail.com>
2025-01-11 18:28:21 +0700

    Debug

--- model/video_music_transformer.py
+++ model/video_music_transformer.py
@@ -1000,8 +1000,7 @@ class VideoMusicTransformer(nn.Module):
         ### Video (SemanticList + SceneOffset + Motion + Emotion) (ENCODER) ###
         # Semantic
         vf_concat = feature_semantic_list.float()
-        for i in range(1, len(feature_semantic_list)):
-            vf_concat = torch.cat( (vf_concat, feature_semantic_list[i].float()), dim=2)            
+                 
         print(vf_concat.shape, feature_scene_offset.unsqueeze(-1).shape)
         # Scene offset
         if not self.scene_embed:

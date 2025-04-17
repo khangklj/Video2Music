865560e9cc8e745cf994ff3cb5409d13c18cd783
Phan Trung Thuận <thuantk000@gmail.com>
2025-04-17 18:24:36 +0700

    update

--- utilities/argument_funcs.py
+++ utilities/argument_funcs.py
@@ -14,10 +14,10 @@ split_path = "split_" + split_ver
 dropout = 0.2
 droptoken = 0.0
 lr = None
-optimizer = 'RAdanW' # Adam / AdamW / RAdam / RAdamW / Lion
+optimizer = 'AdamW' # Adam / AdamW / RAdam / RAdamW / Lion
 auxiliary_loss = True # False / True
 drop_loss = True # False / True
-balancing = True # True / False
+balancing = False # True / False
 
 def parse_train_args():
     parser = argparse.ArgumentParser()
